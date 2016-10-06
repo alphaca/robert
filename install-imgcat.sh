a50a57493475747c7a5eb92f96e0431f054aea10
alphaca bang <alphaca.bang@gmail.com>
2016-10-06 16:38:18 +0900

    Create install-imgcat.sh

---
+++ install-imgcat.sh
@@ -0,0 +1,3 @@
+sudo curl -o /usr/local/bin/imgcat -O https://raw.githubusercontent.com/gnachman/iTerm2/master/tests/imgcat && sudo chmod +x /usr/local/bin/imgcat
+
+#from https://gist.github.com/wesbos/eac5f93478002312db1f
